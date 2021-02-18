87db25e1b8c8a13b26469eafdbed6e0c85f416c1
AadyaMisra <78533258+AadyaMisra@users.noreply.github.com>
2021-02-18 16:01:44 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# DesingingAlogorithm
-CreatingALibaryCode
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,32 @@
+
+var movingRect,fixedRect
+
+
+
+function setup() {
+
+
+  createCanvas(800,400);
+  movingRect=createSprite(400, 200, 50, 80);
+  fixedRect=createSprite(200, 200, 80, 50);
+}
+
+function draw() {
+  background(0); 
+
+  movingRect.x=mouseX
+  movingRect.y=mouseY 
+
+  if(movingRect.x-fixedRect.x<(movingRect.width+fixedRect.width)/2 &&
+     fixedRect.x-movingRect.x<(movingRect.width+fixedRect.width)/2 &&
+     movingRect.y-fixedRect.y<(movingRect.height+fixedRect.height)/2 &&
+     fixedRect.y-movingRect.y<(movingRect.height+fixedRect.height)/2){
+
+
+     movingRect.shapeColor="red"
+  }
+  else{
+    movingRect.shapeColor="blue"
+  }
+  drawSprites();
+}
